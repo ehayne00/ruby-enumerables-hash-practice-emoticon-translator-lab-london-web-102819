91000834245ce9b4ef02a230ad9b79eaa13f3f92
Emily Haynes <ehayne00@hotmail.com>
2019-10-23 14:33:51 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,30 @@
-# require modules here
+require "pry"
+require "yaml"
 
-def load_library
-  # code goes here
+def load_library(file_path)
+  table = YAML.load_file(file_path)
+    result = {"get_meaning" => {}, "get_emoticon" => {}}
+    table.each do |meaning, emoticons|
+      result["get_meaning"][emoticons[1]] = meaning
+      result["get_emoticon"][emoticons[0]] = emoticons[1]
+    end
+    result
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file_path = './lib/emoticons.yml', english_emoticon)
+  table = load_table(file_path)
+  if table["get_emoticon"].include?(english_emoticon)
+    table["get_emoticon"][english_emoticon]
+  else
+    "Sorry, that emoticon was not found"
+  end
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(file_path = './lib/emoticons.yml', japanese_emoticon)
+  table = load_table(file_path)
+  if table["get_meaning"].include?(japanese_emoticon)
+    table["get_meaning"][japanese_emoticon]
+  else
+    "Sorry, that emoticon was not found"
+  end
 end
