c1a4cd72531e3a4ab7834790cc22d2166365c062
Emily Haynes <ehayne00@hotmail.com>
2019-10-23 14:36:30 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,8 +22,8 @@ end
 
 def get_english_meaning(file_path = './lib/emoticons.yml', japanese_emoticon)
   library = load_library(file_path)
-  if table["get_meaning"].include?(japanese_emoticon)
-    table["get_meaning"][japanese_emoticon]
+  if library["get_meaning"].include?(japanese_emoticon)
+    library["get_meaning"][japanese_emoticon]
   else
     "Sorry, that emoticon was not found"
   end
